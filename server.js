088fc3a78e5cb41cd92c069ef802f7d63a0c0e1a
Hassan Amiri <hassanamiri.ai@gmail.com>
2026-01-10 14:27:25 +0100

    initialize servr setup: database conect ..etc

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 const app = require("./app");
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(function () {
+    console.log("succesfully connected to the database");
+
+    app.listen(3000, () => console.log("Server running on port 3000"));
+  })
+  .catch(function (err) {
+    console.error("error connecting to the database:", err);
+  });
