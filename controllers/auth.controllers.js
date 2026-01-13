054be958de49669fe4f6aafc04a795a2d1ccb9e4
Hassan Amiri <hassanamiri.ai@gmail.com>
2026-01-13 17:31:04 +0100

    add: write the login logic in auth.controllers.js

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const User = require("../models/user.model");
 exports.register = async (req, res) => {
   try {
@@ -8,5 +11,37 @@ exports.register = async (req, res) => {
     res.status(201).json({ message: "Registration successful", user: newUser });
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne(username);
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized; invalid username." });
+    }
+    const isMatch = await bcrypt.compare(user.password, password);
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized; invalid username." });
+    }
+    const payload = {
+      id: user._id,
+      username: user.username,
+      admin: user.isAdmin,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ message: "user login succesful", token: token });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Try again later, An internal server error occured " });
   }
 };
