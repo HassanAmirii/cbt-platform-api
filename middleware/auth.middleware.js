8c91a474792851aed6b70e3a60786dff372a1aa7
Hassan Amiri <hassanamiri.ai@gmail.com>
2026-01-14 15:42:08 +0100

    write: the middleware for token verification

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+//handle token verification:
+exports.verifyToken = async (req, res, next) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    if (!authHeader) {
+      return res
+        .status(401)
+        .json({ message: "Authentication required but missing !" });
+    }
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "bad request, invalid token." });
+  }
+};
