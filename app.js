45b5fb8db4db941c3139bcc9e32c878023e31d3d
Hassan Amiri <hassanamiri.ai@gmail.com>
2026-01-14 16:01:16 +0100

    define: auth.route path into main app

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 app.use(express());
+
+app.use("/auth", require("./routes/auth.routes"));
+
 module.exports = app;
