2c262d4c2720bcfa0f33f5c6beff0745b8b0db46
Hassan Amiri <hassanamiri.ai@gmail.com>
2026-01-14 16:05:57 +0100

    add: port variable to server.js and .env

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const app = require("./app");
+const port = process.env.PORT || 3000;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(function () {
     console.log("succesfully connected to the database");
 
-    app.listen(3000, () => console.log("Server running on port 3000"));
+    app.listen(port, () => console.log("Server running on port 3000"));
   })
   .catch(function (err) {
     console.error("error connecting to the database:", err);
