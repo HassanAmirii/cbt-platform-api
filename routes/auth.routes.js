88866976ce329c011cc7e581237e56389c6643c5
Hassan Amiri <hassanamiri.ai@gmail.com>
2026-01-15 10:23:14 +0100

    fix: typo in login route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const authController = require("../controllers/auth.controllers");
 
 router.post("/register", authController.register);
-router.post("login", authController.login);
+router.post("/login", authController.login);
 
 module.exports = router;
