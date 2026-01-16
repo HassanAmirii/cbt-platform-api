1e9e334f6ce99e004a18aaaf89aa420714553422
Hassan Amiri <hassanamiri.ai@gmail.com>
2026-01-16 16:44:11 +0100

    add: comment to the token verification process

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 //handle token verification:
 exports.verifyToken = async (req, res, next) => {
+  //1. check if authorization header exist
   try {
     const authHeader = req.headers["authorization"];
     if (!authHeader) {
@@ -10,7 +11,9 @@ exports.verifyToken = async (req, res, next) => {
         .status(401)
         .json({ message: "Authentication required but missing !" });
     }
+    //2. extract the token from the jwt envelope
     const token = authHeader.split(" ")[1];
+    //3. recompute a new signatire and see if it matched / valid compare with the extracted token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
