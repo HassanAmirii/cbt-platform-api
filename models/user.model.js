e1939112d0b990e9014a7dcb72da9a3df497b5a2
Hassan Amiri <hassanamiri.ai@gmail.com>
2026-01-27 18:53:32 +0100

    rewrite: user model schema with presave fucntion to hash plane password

---
+++ models/user.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      minlength: 4,
+      trim: true,
+    },
+    email: {
+      unique: true,
+      required: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    level: {
+      required: true,
+      type: [String],
+      enum: ["100", "200", "300", "400"],
+    },
+    department: {
+      required: true,
+      type: [String],
+      enum: ["computer science"],
+    },
+    isAdmin: { type: Boolean, required: true, default: false },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+module.exports = mongoose.model("User", userSchema);
