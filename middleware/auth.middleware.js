9dcdd43e5522ce2e1903039103352be9664ce635
Hassan Amiri <hassanamiri.ai@gmail.com>
2026-02-12 15:35:25 +0100

    Fix: implement robust auth header verification

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -5,10 +5,8 @@ exports.verifyToken = async (req, res, next) => {
   //1. check if authorization header exist
   try {
     const authHeader = req.headers["authorization"];
-    if (!authHeader) {
-      return res
-        .status(401)
-        .json({ message: "Authentication required but missing !" });
+    if (!authHeader || authHeader.startsWith("Bearer")) {
+      return res.status(401).json({ message: "missing or invalid scheme!" });
     }
     //2. extract the token from the jwt envelope
     const token = authHeader.split(" ")[1];
