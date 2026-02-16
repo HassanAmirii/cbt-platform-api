05749175fb2a9c9dfba230dee10df162f33ade3b
Hassan Amiri <hassanamiri.ai@gmail.com>
2026-02-16 11:07:53 +0100

    add code plan

---
+++ controllers/exam.controllers.js
@@ -0,0 +1,7 @@
+//import data/question.json
+//get 10 question base on selected ( dept, level, topic, courcode)
+//randomize question and output
+//start exam within defined time
+//submit useranswerIndex and compare with actual question index
+//conpute score percentage and display result
+const getQuestions = require();
