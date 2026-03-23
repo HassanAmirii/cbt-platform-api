abc41bbf2363954c445182bd96e7055bcdecd753
Hassan2bit <hassanamiri.ai@gmail.com>
2026-03-23 19:13:11 +0100

    feat: implemented an exponential back off to handle db connection retries within max time and eventually give up to process.exit 1 if failed

--- server.js
+++ server.js
@@ -2,13 +2,29 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const app = require("./app");
 const port = process.env.PORT || 3000;
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(function () {
-    console.log("succesfully connected to the database");
-    app.listen(port, () => console.log(`Server running on port ${port}`));
-  })
-  .catch(function (err) {
-    console.error("error connecting to the database:", err);
-    process.exit(1);
-  });
+const options = {
+  autoIndex: false,
+  connectTimeoutMS: 10000,
+  socketTimeoutMS: 30000,
+  family: 4,
+};
+
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+async function connectWithRetry() {
+  let delay = 1000;
+  for (let retry = 0; retry < 5; retry++) {
+    try {
+      await mongoose.connect(process.env.MONGO_URI, options);
+      console.log("succesfully connected to the database");
+      app.listen(port, () => console.log(`Server running on port ${port}`));
+      return;
+    } catch (error) {
+      console.error("error connecting to the database:", error);
+      await wait(delay);
+      delay *= 2;
+    }
+  }
+  process.exit(1);
+}
+
+connectWithRetry();
