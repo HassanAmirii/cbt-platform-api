48a2c99c5a7415ab8f288c04fc93921870c7fe3f
Hassan2bit <hassanamiri.ai@gmail.com>
2026-03-24 19:56:00 +0100

    add auth middleware to protect route + get access to req.user in the payload

--- routes/v1/exam.routes.js
+++ routes/v1/exam.routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
 const router = express.Router();
-
+const auth = require("../../middleware/auth.middleware");
 const examController = require("../../controllers/exam.controllers");
 
-router.post("/start-exam", examController.startExam);
-router.post("/submit-exam", examController.submitExam);
+router.post("/start-exam", auth, examController.startExam);
+router.post("/submit-exam", auth, examController.submitExam);
 module.exports = router;
