fb34c29f44e17bd36326b14b18b643e758574f0d
Hassan2bit <hassanamiri.ai@gmail.com>
2026-03-30 19:51:12 +0100

    fix: correct dotenv config invocation in question seeder

--- src/seeders/question.seeder.js
+++ src/seeders/question.seeder.js
@@ -1,4 +1,4 @@
-require("dotenv").config;
+require("dotenv").config();
 const mongoose = require("mongoose");
 const Question = require("../models/question.model");
 const questions = require("../../data/questions.json");
