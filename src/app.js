6fe3767324310dc6448503322003406ddb9f70fa
Hassan2bit <hassanamiri.ai@gmail.com>
2026-04-02 10:12:11 +0100

    feat: add user routes to API v1

--- src/app.js
+++ src/app.js
@@ -58,6 +58,8 @@ routes mount
 */
 const authRoutes = require("./routes/v1/auth.routes");
 const examRoutes = require("./routes/v1/exam.routes");
+const userRoutes = require("./routes/v1/user.routes");
+app.use("/api/v1", userRoutes);
 app.use("/api/v1", authRoutes);
 app.use("/api/v1", examRoutes);
 
