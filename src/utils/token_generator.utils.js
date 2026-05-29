c471c57e6e549142c8d1d2252c042073a9793e17
Hassan2bit <hassanamiri.ai@gmail.com>
2026-05-29 17:25:12 +0100

    feat: add semester validation to registration schema and include semester in JWT token generation

--- src/utils/token_generator.utils.js
+++ src/utils/token_generator.utils.js
@@ -7,6 +7,7 @@ exports.generateToken = (user) => {
       department: user.department,
       admin: user.isAdmin,
       level: user.level,
+      semester: user.semester,
     },
     process.env.JWT_SECRET,
     { expiresIn: "1h" },
